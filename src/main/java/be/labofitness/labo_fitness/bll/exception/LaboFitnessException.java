b8336985e98f7dee0c0c77fa5faeee78b0d7a647
MFivez <fivezmauritciopro@gmail.com>
2024-05-17 10:49:59 +0200

    ==> feature ExceptionManaging (controllerAdivosr) ==> feature: GloablError = LaboFitnessException

---
+++ src/main/java/be/labofitness/labo_fitness/bll/exception/LaboFitnessException.java
@@ -0,0 +1,9 @@
+package be.labofitness.labo_fitness.bll.exception;
+
+
+public class LaboFitnessException extends RuntimeException {
+
+    public LaboFitnessException(String message) {
+        super(message);
+    }
+}
