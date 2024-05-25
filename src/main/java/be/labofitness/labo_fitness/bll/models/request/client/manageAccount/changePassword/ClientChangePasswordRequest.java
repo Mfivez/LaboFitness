9b7d254f7f40d6be223488df54e33539abf33245
Rulens Thomas <rulens.thomas@gmail.com>
2024-05-25 22:05:39 +0200

    feature : - client manage account / change password - coach manage account / change password - physiotherapist manage account / change password
    bonus : custom validador  : ValidatorMessageCustom, CustomEmailValidator, CustomPasswordValidator
    
    + hotfix

--- src/main/java/be/labofitness/labo_fitness/bll/models/request/client/manageAccount/changePassword/ClientChangePasswordRequest.java
+++ src/main/java/be/labofitness/labo_fitness/bll/models/request/client/manageAccount/changePassword/ClientChangePasswordRequest.java
@@ -1,12 +1,12 @@
 package be.labofitness.labo_fitness.bll.models.request.client.manageAccount.changePassword;
 
-import be.labofitness.labo_fitness.il.utils.annotations.extendedEmailvalidator.CustomEmailValidator;
+import be.labofitness.labo_fitness.il.utils.annotations.validatorPassword.CustomPasswordValidator;
 
 public record ClientChangePasswordRequest (
 
         String oldPassword,
 
-        @CustomEmailValidator
+        @CustomPasswordValidator
         String newPassword
 ){
 }
